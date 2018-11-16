c5890aec4e8172afb80dff1c2a133d0d136ab919
danilads <vfhn3fghtkm>
2018-11-16 19:29:29 +0300

    update

--- 001 webpack/011 semantic-ui-react/components/Block_MainPage.js
+++ 001 webpack/011 semantic-ui-react/components/Block_MainPage.js
@@ -1,20 +1,27 @@
 import React,{Fragment} from 'react';
 import Filter from '../primitives/Filter';
-import { Visibility } from 'semantic-ui-react';
+import {Visibility} from 'semantic-ui-react';
+
 
 class Block_MainPage extends React.PureComponent {
 	
 	state = {
-		
+		content:[1,2,3,4,5,6,7,8,9,10]
 	};
-
+	showMore=()=>{
+		console.log('show more');
+		let anotherArr = [11,12,13,14,15,16,17,18,19,20]
+		this.setState({content:this.state.content.concat(anotherArr)});
+	};
   	render() {
 		return (
 			<div className={"Block_Filter"}>
-				<Visibility>
-					<Fragment>
-						<Filter>hello</Filter>
-					</Fragment>
+				<Visibility
+					onBottomVisible={()=>{this.showMore()}}
+				>
+					{this.state.content.map((it,ind)=>{
+						return <div key={ind} style={{padding:"20px 0",border:'1px dashed gray'}}>{it}</div>
+					})}
 				</Visibility>
 			</div>
 		);
