a9b798f35197cbb6074c9060bf3eb00f7be6f697
danilads <vfhn3fghtkm>
2019-01-14 18:08:28 +0300

    update

---
+++ 001 webpack/014 crypto-js/components/Block_MainPage.js
@@ -0,0 +1,38 @@
+import React,{Fragment} from 'react';
+
+
+import Crypt from 'crypto-js';
+import './Block_MainPage.scss';
+
+class Block_MainPage extends React.PureComponent {
+	
+	state = {
+	};
+
+  	render() {
+		//AES
+		/*** encrypt */  
+		let AESe = Crypt.AES.encrypt("plaintext", "Secret Passphrase");  
+		console.log('AES encrypt',AESe);
+
+		/*** decrypt */  
+		let AESd  = Crypt.AES.decrypt(AESe, "Secret Passphrase");  
+		console.log('AES decrypt',AESd);
+		
+		//  get text
+		var originalText = AESd.toString(Crypt.enc.Utf8);
+		console.log('AES text',originalText);
+		
+		return (
+			<div className={"Block_Filter"}>
+				hello
+			</div>
+		);
+
+  	}
+
+}
+
+
+
+export default Block_MainPage;
