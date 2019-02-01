c178bf214182c59d18443b63c1e57fcf7b538f52
danilads <vfhn3fghtkm>
2019-02-01 18:20:50 +0300

    updt

---
+++ 001 webpack/013 react-select/components/Ex1.js
@@ -0,0 +1,23 @@
+import React,{Fragment} from 'react';
+
+
+class Ex1 extends React.PureComponent {
+	
+	state = {
+		
+	};
+
+  	render() {
+		return (
+			<div className={"Block_Filter"}>
+				hello
+			</div>
+		);
+
+  	}
+
+}
+
+
+
+export default Ex1;
