d5b17cb6c753c774aa537eec0dc3d23930176578
danilads <vfhn3fghtkm>
2019-02-09 02:05:27 +0300

    upd

---
+++ refactor.js
@@ -0,0 +1,41 @@
+toggleElem = isOpened => {
+    const {state: {isOpened: prevIsOpened}, props: {cbOpen, cbClose}}} = this;
+  
+    if (!isBoolean(isOpened))
+      isOpened = !prevIsOpened;
+  
+  
+    this.setState({
+      isOpened
+    });
+  
+    const cb = isOpened ? cbOpen : cbClose;
+  
+    cb && cb();
+  }
+
+
+  openCloseFunc=(e)=>{
+    //if send atr
+    //open
+    if(e===true){
+        this.setState({isOpened:this.state.e});
+        this.props.cbOpen && this.props.cbOpen();
+      }
+      //close
+      else if(e===false){
+        this.setState({isOpened:this.state.e});
+        this.props.cbClose && this.props.cbClose();
+      }
+      //toggle
+      else{
+        this.setState({isOpened:!this.state.isOpened});
+        if(this.state.isOpened){
+          this.props.cbClose && this.props.cbClose();
+        }
+        else{
+          this.props.cbOpen && this.props.cbOpen();
+        }
+      }
+    };
+  
