f92e541a7e601f9b2b8feb2cebcb7a8f8ff46f8f
danilads <vfhn3fghtkm>
2019-04-04 12:31:05 +0300

    updt

--- 001 webpack/500 primitives/components/InputTextWithSpaces.js
+++ 001 webpack/500 primitives/components/InputTextWithSpaces.js
@@ -11,24 +11,45 @@ class InputTextWithSpaces extends PureComponent {
   };
 
   state={
+    //кейсы isDirty -22-22
     value: "",
+    //value: "-"
+    //value: "-2"
+    //value: "-22"
+    //value: "-22-"
+    //value: "-22-33"
+    //value: "-22-33--"
+    //value: "d22-ed"
+    //value: "d22-ed"
+
+    //кейсы Clear -22-22
+    //value: "",
+    //value: "2"
+    //value: "22"
+    //value: "2222"
+    //value: "-22-2"
+    //value: "-22-33"
+    //value: "-22-33--"
+    //value: "d22-ed"
+    //value: "d22-ed22"
   }
+
   containerRef = {};
-  dirtyValue = "";
+  clearValue = "";
 
   render() {
     let {className} = this.props;
     let value = this.state.value;
-    let result = reactRefModifyStr([{reg:"^[0-9]+$",symbol:"@"}], this.containerRef, "ᾩᾩ-ᾩᾩ", value, false);
-    this.dirtyValue = result.dirtyVal;
+    let result = reactRefModifyStr([{reg:"^[0-9]+$",symbol:"@"}], this.containerRef, "-@@-@@", value, false, 0, true);
+    this.clearValue = result.clearVal;
 
-    console.log('--state value', this.state.value);
-    console.log('--dirtyValue', this.dirtyValue);
+    // console.log('--state value', this.state.value);
+    // console.log('--clearValue', this.clearValue);
  
 
     return (
         <div className={classNames('InputTextWithSpaces', className)}>
-            <input type="text" value={result.clearVal} onChange={(e)=>this.setState({value:e.target.value})} ref={(el)=>this.containerRef=el}/>
+            <input type="text" value={result.dirtyVal} onChange={(e)=>this.setState({value:e.target.value})} ref={(el)=>this.containerRef=el}/>
         </div>
     );
   }
--- 001 webpack/500 primitives/utils/utils.js
+++ 001 webpack/500 primitives/utils/utils.js
@@ -5,34 +5,43 @@
 // и запись в value (this.setState({value:e.target.value})) на запись в редакс
 
 // Как подключать через state:
-// state={ value: "", dirtyValue: "", clearValue: "",} 
+// state={ value: ""} 
 // containerRef = {};
-// dirtyValue = "";
+// clearValue = "";
 //
 // render:
 // let value = this.state.value;
 // let result = reactRefModifyStr([{reg:"^[0-9]+$",symbol:"@"}], this.containerRef, "@@-@@", value, false);
-// this.dirtyValue = result.dirtyVal;
+// this.clearValue = result.clearVal;
+//
+// <input type="text" value={result.dirtyVal} onChange={(e)=>this.setState({value:e.target.value})} ref={(el)=>this.containerRef=el}/>
+
 
 
 // specSymbols - массив типа: [{reg:"^[0-9]+$",symbol:"@"}, {reg:"^[a-z]+$",symbol:"&"}, {reg:".*",symbol:"!"}]
 //      - model должна выглядеть так:
 //      "@@@@" => "4219" , "@@ @@-@@" => "34 54-13" , "@@&&" => "32df" , "!!! !!!" => "e@3 *,!"
 //      - symbol не должны повторятся и !Указанные символы невозможно будет ввести, поэтому можно использовать такие
-//      जावास्क्रिप्टĦĔĽĻŎᾩ
+//      जावास्क्रिप्टĦĻŎᾩĽĔ
 
 // ref - присылаем рефку текущего инпута
 
 // model - типа: "@@ @@-@@-@@"
 //      должна состоять из символов specSymbols
 
-// value - значение
+// value - значение зависит от isDirty
 
 // deafaultCompleteMode = 1
-// 0 "" //автозаполнение после ввода первого символа "+3"
-// 1 "+___(__)___-__-__"
+//      0 автозаполнение после ввода первого символа "" => "+3"
+//      1 автозаполнение после ввода первого символа(но все символы до первого изменяемого - сразу видны) "+" => "+3"
+//      2 "+___(__)___-__-__" //сразу все заплняется(пустые места - maskSymbol)
+
+// isDirty - если в value приходят:
+//      грязное значение (+375(44)764-43-11) то - true
+//      чистое значение (375447644311) то - false
 
 // Необязательный аргумент
+// (если не прислать будет пробел - " ")
 // maskSymbol - символ который будет заполнять пустые места, если "_" => +___(__)___-__-__
 
 // Необязательный аргумент
@@ -43,10 +52,42 @@
 
 // если model - "+@@@(@@) @@@ @@ @@"
 // return - данная функция возвращает объект {clearVal: "375294312334", dirtyVal: "+375(29)431-23-34"}
-export function reactRefModifyStr (specSymbols, ref, model, value, deafaultCompleteMode, maskSymbol, isInfinite){
+export function reactRefModifyStr (specSymbols, ref, model, value, deafaultCompleteMode, isDirty, maskSymbol, isInfinite){
+    // проверяем валидность данных (потом доделать) regex проверяем так(let reg = new RegExp('^[a-z]+$')) заворачиваем в try catch - не создаться вылетит ошибка
+    
+
+    // работа с движком
+    // 01 определяем модель
+    // 02 определяем разрешенные символы
+    // this.containerRef.focus(); 
+
+    console.log('--в каком месте стоит курсор после изменения', ref.selectionStart);
+
+
+    let innerValidValue = "";
+
+
+    //Если приходят невалидные данные приводим их к форме
+    let cnt = 0; //счетчик перебора value
+    for(let i=0;i<model.length;i++){
+
+    }
+    
+
+    
+    if(isDirty){
+        if(deafaultCompleteMode===0){
+            
+        }
+    }
+    else{
+
+    }
+    
+    
     let drtVal;
     let clVal;
-    console.log('--model',model);
+    //console.log('--model',model);
 
     drtVal = value;
     clVal = value;
