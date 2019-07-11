6ff93fe722b70cec0644e3b8f17ed41d171ca7ea
danilads <vfhn3fghtkm>
2019-07-11 17:40:26 +0300

    updt

--- 001 webpack/015 Antd/components/Block_MainPage.js
+++ 001 webpack/015 Antd/components/Block_MainPage.js
@@ -1,12 +1,13 @@
 import React,{Fragment} from 'react';
 
 import AntdTable from './AntdTable';
+import AntdMenu from './AntdMenu';
 
 
 
 class Block_MainPage extends React.PureComponent {
 	state={
-		pos:1,
+		pos:2
 	}
 	
   	render() {
@@ -17,12 +18,13 @@ class Block_MainPage extends React.PureComponent {
 				}}>AntdTable</button>
 				<button onClick={()=>{
 					this.setState({pos:2});
-				}}>Popup</button>
+				}}>AntdMenu</button>
 				<button onClick={()=>{
 					this.setState({pos:3});
 				}}>Modal</button>
 
 				{this.state.pos===1&&<AntdTable/>}
+				{this.state.pos===2&&<AntdMenu/>}
 			
 				
 			</div>
