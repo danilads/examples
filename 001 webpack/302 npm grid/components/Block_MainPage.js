c48d835d3776af20fdbadd61192e99706e4b1133
danilads <vfhn3fghtkm>
2019-08-08 17:30:34 +0300

    updt

---
+++ 001 webpack/302 npm grid/components/Block_MainPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+class Block_MainPage extends React.PureComponent {
+	
+	state = {
+		text:''
+	};
+
+  	render() {
+
+		return (
+			<div>
+				<div>
+					hello
+				</div>
+			</div>
+		);
+
+  	}
+
+}
+
+
+export default Block_MainPage;
---
+++ 001 webpack/302 npm grid/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const devMode = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  //устанавливается в packaje.json (нужен cross-env)
+ mode: process.env.NODE_ENV,
+ //mode: 'development',
+ //mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin()
+    ]
+  },
+  devtool: devMode?'inline-source-map':undefined, //'source map' - для того чтобы в chrome в вкладке source можно было читать несобранный код
+  devServer: {
+    inline:true,
+    //port: 8080,
+    historyApiFallback: true, //для react-router-dom (cannot get какой-то url)
+  },
+  entry: {
+		main: ['babel-polyfill', './app.js'] //для работы async / await
+	},
+  output:{ 
+	path: __dirname, // путь к каталогу выходных файлов
+	filename: "bundle.js",  // название создаваемого файла
+	}, 
+  module: {
+    rules: [
+		{
+			test: /\.js$/,
+			use: {
+				loader: "babel-loader"
+		}
+		},
+      {
+        test: /\.(scss|css)$/,
+    use: [
+        MiniCssExtractPlugin.loader,
+        {
+            loader: "css-loader",
+            options: {
+                minimize: {
+                    safe: true
+                }
+            }
+        },
+        {
+            loader: "sass-loader",
+            options: {}
+        }
+    ]
+      }
+    ]
+  },
+  plugins: [
+	new MiniCssExtractPlugin({
+		// Options similar to the same options in webpackOptions.output
+		// both options are optional
+		filename: "bundle.css"
+	  })
+	]
+};
