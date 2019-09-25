6b670a617e7b43d04564ee105b2c8476a5310227
danilads <vfhn3fghtkm>
2019-09-25 12:24:00 +0300

    updt

--- 001 webpack/207 minimize (preact)/App.js
+++ 001 webpack/207 minimize (preact)/App.js
@@ -9,6 +9,5 @@ import './scss/main.scss';
 ////----components
 import Main from './components/Main';
 
-// var doc = document.getElementById('container');
-// doc.innerHTML = "<div>hello</div>";
+
 render(<Main/>, document.getElementById('container'));
--- 001 webpack/207 minimize (preact)/webpack.config.babel.js
+++ 001 webpack/207 minimize (preact)/webpack.config.babel.js
@@ -4,7 +4,6 @@ import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 const devMode = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  //устанавливается в packaje.json (нужен cross-env)
  mode: process.env.NODE_ENV,
  //mode: 'development',
  //mode: 'production',
@@ -13,18 +12,18 @@ module.exports = {
       new UglifyJsPlugin()
     ]
   },
-  devtool: devMode?'inline-source-map':undefined, //'source map' - для того чтобы в chrome в вкладке source можно было читать несобранный код
+  devtool: devMode?'inline-source-map':undefined, //'source map'
   devServer: {
     inline:true,
     //port: 8080,
-    historyApiFallback: true, //для react-router-dom (cannot get какой-то url)
+    historyApiFallback: true, 
   },
   entry: {
 		main: './App.js'
 	},
   output:{ 
-	path: __dirname, // путь к каталогу выходных файлов
-	filename: "bundle.js",  // название создаваемого файла
+	path: __dirname, 
+	filename: "bundle.js", 
   }, 
   resolve: {
     extensions: ['*', '.js', '.jsx']
@@ -59,8 +58,6 @@ module.exports = {
   },
   plugins: [
 	new MiniCssExtractPlugin({
-		// Options similar to the same options in webpackOptions.output
-		// both options are optional
 		filename: "bundle.css"
 	  })
 	]
