d19e0fb32f622a564874554f81cb1660caeae181
danilads <vfhn3fghtkm>
2019-10-07 13:23:27 +0300

    upd

---
+++ 001 webpack/024 lodash/components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Main extends React.PureComponent {
+	
+	
+  	render() {
+		
+		return (
+			<div>
+			    LODASH
+			</div>
+		);
+
+  	}
+
+}
+
+
+export default Main;
