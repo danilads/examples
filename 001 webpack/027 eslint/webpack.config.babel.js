44ece632ca0728460a3d3f4530aec21c9caeeeb7
danilads <vfhn3fghtkm>
2019-11-01 14:22:34 +0300

    updt

---
+++ 001 webpack/027 eslint/webpack.config.babel.js
@@ -0,0 +1,84 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const devMode = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  //устанавливается в packaje.json (нужен cross-env)
+ mode: process.env.NODE_ENV,
+ //mode: 'development',
+ //mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin()
+    ]
+  },
+  devtool: devMode?'inline-source-map':undefined, //'source map' - для того чтобы в chrome в вкладке source можно было читать несобранный код
+  devServer: {
+    inline:true,
+    //port: 8080,
+    historyApiFallback: true, //для react-router-dom (cannot get какой-то url)
+  },
+  entry: {
+		main: ['babel-polyfill', './app.js'] //для работы async / await
+	},
+  output:{ 
+    path: __dirname + '/public', // путь к каталогу выходных файлов
+    publicPath: '/',
+	  filename: "bundle.js",  // название создаваемого файла
+  }, 
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+		{
+			test: /\.(js|jsx)?$/,
+			use: {
+				loader: "babel-loader"
+		}
+		},
+      {
+        test: /\.(scss|css)$/,
+    use: [
+        MiniCssExtractPlugin.loader,
+        {
+            loader: "css-loader",
+            options: {
+                minimize: {
+                    safe: true
+                }
+            }
+        },
+        {
+            loader: "sass-loader",
+            options: {}
+        }
+    ]
+      }
+    ]
+  },
+  plugins: [
+    // при запуске открывает страницу с размерами пакетов и компонентов
+    // new BundleAnalyzerPlugin(),
+    // //копирует папки fonts/images
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: 'fonts',
+    //     to: 'fonts'
+    //   }
+    // ]),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "bundle.css"
+    }),
+    //копирует index.html в папку
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: false
+    })
+	]
+};
