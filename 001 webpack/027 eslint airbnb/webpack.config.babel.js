fcfb3344467057cb2cf50af7b0582939b3bf9893
danilads <vfhn3fghtkm>
2019-11-01 16:50:46 +0300

    upd

--- 001 webpack/027 eslint airbnb/webpack.config.babel.js
+++ 001 webpack/027 eslint airbnb/webpack.config.babel.js
@@ -35,9 +35,10 @@ module.exports = {
   module: {
     rules: [
 		{
-			test: /\.(js|jsx)?$/,
+      test: /\.(js|jsx)?$/,
+      exclude: /node_modules/,
 			use: {
-				loader: "babel-loader"
+				loader: ["babel-loader", "eslint-loader"]
 		}
 		},
       {
