44fc7fdde196210902cffd0b679125f0cf808f43
danilads <vfhn3fghtkm>
2019-11-01 17:48:46 +0300

    upd

--- 001 webpack/027 eslint airbnb/components/Main.js
+++ 001 webpack/027 eslint airbnb/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 class Main extends React.PureComponent {
-
+	
   	render() {
-		
+		const n = 123;
+		this.setState({hello:1});
 		return (<div>
-				hello
+				{n}
 			</div>);
 
   	}
