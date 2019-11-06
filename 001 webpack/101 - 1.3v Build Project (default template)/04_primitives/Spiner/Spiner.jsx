825ba45f3aae1e665364636daa48a0f8f1424712
danilads <vfhn3fghtkm>
2019-11-06 17:06:51 +0300

    upd

--- 001 webpack/101 - 1.3v Build Project (default template)/04_primitives/Spiner/Spiner.jsx
+++ 001 webpack/101 - 1.3v Build Project (default template)/04_primitives/Spiner/Spiner.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
-
 import './Spiner.scss';
+
 class Spiner extends React.PureComponent {
-	
-	state = {
-		
-	};
+  state = {
 
-  	render() {
+  };
 
-		return (
-            <div className={"Spiner"}>
-                <div className={"spinner"}>
-                    <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-                </div>
-            </div>
-		);
-
-  	}
-
+  render() {
+    return (
+      <div className={'Spiner'}>
+        <div className={'spinner'}>
+          <div className={'lds-roller'}>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
 
 export default Spiner;
--- 001 webpack/101 - 1.3v Build Project (default template)/App.jsx
+++ 001 webpack/101 - 1.3v Build Project (default template)/App.jsx
@@ -1,47 +1,43 @@
-﻿//react
+﻿// react
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//для работы async / await
-import "babel-polyfill";
+// для работы async / await
+import 'babel-polyfill';
 
-
-//redux
+// redux
 import { Provider } from 'react-redux';
 
-//route
+// route
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-//layout
+// layout
 import Layout from './02_components/Layout/Layout';
 
-//components
+// components
 import PageMain from './01_pages/PageMain/PageMain';
 import PageAbout from './01_pages/PageAbout/PageAbout';
 import PageSome from './01_pages/PageSome/PageSome';
 
-//store
+// store
 import store from './redux/store';
 
 
-//css
+// css
 import './style/main.scss';
 
 
 ReactDOM.render(
-	
-	<Provider store={store}>
-		<BrowserRouter>
-			<Switch>
-				<Route path="/" exact component={PageMain} />
-				<Layout>
-					<Route path="/about" component={PageAbout} />
-					<Route path="/some" component={PageSome} />
-				</Layout>
-			</Switch>
-		</BrowserRouter>
-	</Provider>
-	
-	, document.getElementById('container') 
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path={'/'} exact component={PageMain} />
+        <Layout>
+          <Route path={'/about'} component={PageAbout} />
+          <Route path={'/some'} component={PageSome} />
+        </Layout>
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('container')
 );
-
