c75214a376d8c44a6a5efa931497a523355b1541
danilads <vfhn3fghtkm>
2020-03-22 12:55:36 +0300

    upd

--- 002 ts react-script/002 simple react/src/App.tsx
+++ 002 ts react-script/002 simple react/src/App.tsx
@@ -14,12 +14,36 @@ interface AppProps {
 //   );
 // }
 
-const App: React.FC<AppProps> = props => {
+// const App: React.FC<AppProps> = props => {
+//   return (
+//     <div className="App">
+//       {props.name}
+//     </div>
+//   )
+// }
+
+// class App extends React.Component<AppProps> {
+//   run=()=>{
+//     console.log('hello');
+//   }
+//   render(){
+//     return(
+//         <div className="App" onClick={this.run}>
+//           {this.props.name}
+//         </div>
+//         )
+//     }
+// }
+
+const App: React.FC<AppProps> = (props) => {
+
+  const [some] = React.useState('123');
   return (
-    <div className="App">
+    <div className="App" onClick={()=>console.log(some)}>
       {props.name}
     </div>
   )
 }
+
 
 export default App;
--- 002 ts react-script/002 simple react/src/index.tsx
+++ 002 ts react-script/002 simple react/src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name="hello" />
+    <App name={"hello"} />
   </React.StrictMode>,
   document.getElementById('root')
 );
