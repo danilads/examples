e56aa980fd024e7f857171d4b5e3f21665af7963
danilads <vfhn3fghtkm>
2020-03-23 22:14:37 +0300

    upd

--- 002 ts react-script/002 simple react/src/App.tsx
+++ 002 ts react-script/002 simple react/src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 interface AppProps {
   name: string;
 }
-
+interface AppState {
+  cnt: number;
+}
 
 // const App = (props: AppProps) => {
 //   return (
@@ -36,14 +38,44 @@ interface AppProps {
 // }
 
 const App: React.FC<AppProps> = (props) => {
+  //state
+  const [some,setSome] = React.useState('123');
+  const [text,setText] = React.useState('some');
+  const [obj,setObj] = React.useState([{ text: 'Изучить хуки' }]);
 
-  const [some] = React.useState('123');
+  // Аналогично componentDidMount и componentDidUpdate
+  React.useEffect(() => {
+    //event listener
+    const handler = () => {
+      console.log(window.innerHeight);
+    }
+
+    window.addEventListener('resize', handler);
+  
+    return () => {
+      window.removeEventListener('resize', handler);
+    }
+  });
+
   return (
     <div className="App" onClick={()=>console.log(some)}>
       {props.name}
     </div>
   )
 }
+
+// export class App extends React.Component<AppProps,AppState> {
+//   state={
+//     cnt:0
+//   }
+
+//   render() {
+//     console.log('--state',this.state);
+//     return <div className="App" onClick={()=>this.setState({cnt:this.state.cnt+1})}>
+//           {this.props.name}
+//       </div>
+//   }
+// }
 
 
 export default App;
