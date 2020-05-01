4f2fb92ba3c84a2fdf48f1e358f7f14a7f29ed84
danilads <vfhn3fghtkm>
2020-05-01 18:13:19 +0300

    upd

--- 002 ts react-script/005 styleguidist/styleguide.config.js
+++ 002 ts react-script/005 styleguidist/styleguide.config.js
@@ -1,4 +1,26 @@
 module.exports = {
   defaultExample: true,
-  propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json', { propFilter: { skipPropsWithoutDoc: true } }).parse
+  propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json', { propFilter: { skipPropsWithoutDoc: true } }).parse,
+  template: {
+      head: {
+          links: [
+              {
+                  rel: 'stylesheet',
+                  href: 'public/fonts/fonts.css'
+              }
+          ]
+      }
+  },
+  theme: {
+      fontFamily: {
+          base: 'Roboto'
+      }
+  },
+  styles: {
+      StyleGuide: {
+          '@global body': {
+              fontFamily: 'Roboto'
+          }
+      }
+  }
 };
