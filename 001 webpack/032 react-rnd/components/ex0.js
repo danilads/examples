f20379a9dad5f271273743fc056bd65385e780da
danilads <vfhn3fghtkm>
2020-07-06 20:55:39 +0300

    upd

--- 001 webpack/032 react-rnd/components/ex0.js
+++ 001 webpack/032 react-rnd/components/ex0.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Rnd } from "react-rnd";
+
+const style = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "solid 1px #ddd",
+  background: "#f0f0f0"
+};
 
 class Ex0 extends React.Component {
 
 	render() {
     
 		return (
-			<div>Ex0</div>
+			<div>
+				<div>Ex0</div>
+				<Rnd
+					style={style}
+					default={{
+						width: 200,
+						height: 200,
+						x: 0,
+						y: 0,
+					}}
+					resizeGrid={[20, 20]}
+					dragGrid={[20, 20]}
+				>
+					001
+				</Rnd>
+			</div>
     );
 
 	}
