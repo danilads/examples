39de6b1340a81dc67469cf51d494652eaae74029
danilads <vfhn3fghtkm>
2020-07-08 00:00:46 +0300

    upd

--- 001 webpack/032 react-rnd/components/ex3.js
+++ 001 webpack/032 react-rnd/components/ex3.js
@@ -6,40 +6,58 @@ const style = {
   alignItems: "center",
   justifyContent: "center",
   border: "solid 1px #ddd",
-  background: "#f0f0f0"
+  background: "rgba(10,10,10,0.3)"
 };
 
 class Ex3 extends React.Component {
 	state = {
-    width: 200,
+		STEP: 25,
+    width: 75, // не больше чем RND maxWidth 
     height: 200,
     x: 0,
 		y: 0,
-		isSet: false
+		isSet: false,
 	};
 
-	set=(e)=>{
-		console.log('--e',e);
-		console.log('--eTop',e.target.getBoundingClientRect());
-		console.log('--eX',e.clientX);
-		console.log('--eY',e.clientY);
-		console.log('--e tar',e.target.offsetLeft);
-		console.log('--e tar',e.target.offsetHeight);
-		console.log('---------------');
-	};
+	parentRef = React.createRef();
+	
+	setEvent=(e)=>{
+		if(e.target.isEqualNode(this.parentRef.current)){
+			const {STEP,width} = this.state;
+			const left = e.pageX-(window.scrollX+e.target.getBoundingClientRect().left)+1;
+			let result = Math.floor(left/STEP)*STEP;
+			//не должно быть больше ширины родителя (STEP*25)
+			if ((result+width)>(STEP*25)) {
+				result = STEP*25-width;
+			}
+			this.setState({isSet:true, x: result});
+		}
+	}
+
+	componentDidMount() {
+		document.addEventListener("mousedown", this.setEvent, false);
+	}
+
+	componentWillUnmount(){
+		document.removeEventListener("mousedown", this.setEvent, false);
+	}
 
 	render() {
 		console.log('--state', this.state);
 		// STEP
-		const STEP = 25;
+		const {STEP} = this.state;
 		return (
-			<div style={{margin:'10px' ,width:`${STEP*25}px`, background:'gray', height:'200px'}} onClick={this.set}>
+			<div ref={this.parentRef} style={{margin:'10px' ,width:`${STEP*25}px`, background:'gray', height:'200px'}}>
 				<div>Ex3 - state controlled + onclick set / disappear</div>
 				{this.state.isSet && <Rnd
 					style={style}
-
+					onClick={(e)=>{
+						e.stopPropagation();
+					}}
 					bounds="parent"
 
+					maxWidth={STEP*3}
+					minWidth={STEP}
 					size={{ width: this.state.width, height: this.state.height }}
 					position={{ x: this.state.x, y: this.state.y }}
 					
@@ -56,6 +74,7 @@ class Ex3 extends React.Component {
             this.setState({ x: rounded, y: 0 });
           }}
           onResizeStop={(e, direction, ref, delta, position) => {
+						e.stopPropagation();
             this.setState({
               width: ref.style.width,
               height: ref.style.height,
