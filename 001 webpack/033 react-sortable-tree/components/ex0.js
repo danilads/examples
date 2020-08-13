df5d501e0ba3c57b5f0ef979939766136601132a
danilads <vfhn3fghtkm>
2020-08-13 17:02:34 +0300

    upd

---
+++ 001 webpack/033 react-sortable-tree/components/ex0.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import {TouchBackend} from 'react-dnd-touch-backend';
+import { SortableTreeWithoutDndContext as SortableTree } from 'react-sortable-tree';
+
+// !нужно добавить стили библиотеки
+import './style.css';
+
+const isTouchDevice = !!('ontouchstart' in window || navigator.maxTouchPoints);
+const dndBackend = isTouchDevice ? TouchBackend : HTML5Backend;
+
+class Ex0 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      treeData: [
+        { title: 'Chicken', expanded: true, children: [{ title: 'Egg' }] },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Работает тач (но нужно перезагрузить страницу)</h2>
+        <DndProvider backend={dndBackend}>
+          <div>
+            <span>
+              This is {!isTouchDevice && 'not '}a touch-supporting browser
+            </span>
+
+            <div style={{ height: 300 }}>
+              <SortableTree
+                treeData={this.state.treeData}
+                onChange={treeData => this.setState({ treeData })}
+                
+              />
+            </div>
+          </div>
+        </DndProvider>
+      </div>
+    );
+  }
+}
+
+export default Ex0;
