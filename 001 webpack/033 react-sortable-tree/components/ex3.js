20cb787ce60ee82bd816dafb9eb3727c3fc93886
danilads <vfhn3fghtkm>
2020-08-16 13:23:34 +0300

    upd

--- 001 webpack/033 react-sortable-tree/components/ex3.js
+++ 001 webpack/033 react-sortable-tree/components/ex3.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import {TouchBackend} from 'react-dnd-touch-backend';
-import { SortableTreeWithoutDndContext as SortableTree } from 'react-sortable-tree';
+import { toggleExpandedForAll, SortableTreeWithoutDndContext as SortableTree } from 'react-sortable-tree';
+import FileExplorerTheme1 from 'react-sortable-tree-theme-file-explorer';
+import FileExplorerTheme2 from 'react-sortable-tree-theme-material-ui';
 
 // нужно добавить стили библиотеки
-import './FullWidth.scss'; // !!! стили для все ширины
+// import './FullWidth.scss'; // !!! стили для все ширины
 import './style.css';
 
 
@@ -13,22 +15,86 @@ const isTouchDevice = !!('ontouchstart' in window || navigator.maxTouchPoints);
 const dndBackend = isTouchDevice ? TouchBackend : HTML5Backend;
 
 class Ex3 extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      treeData: [
-        { title: <div>hello<br/>hello</div>, expanded: true, children: [{ title: 'Egg' }] },
-      ],
-    };
+  state = {
+    treeData: [
+      {
+        id: 'a1',
+        title: 'One',
+        expanded: true,
+        height: 100,
+        children:[{id: 'a2', title: 'Two', height: 70}]
+      }
+      // {
+      //   title: 'Tree',
+      //   expanded: true,
+      //   height: 100,
+      // },
+      // {
+      //   title: 'Four',
+      //   expanded: true,
+      //   height: 100,
+      // }
+    ],
   }
 
+  container = React.createRef();
+
+  componentDidMount(){
+    this.reCountHeight();
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(this.state.treeData !== prevState.treeData){
+      this.reCountHeight();
+    }
+  }
+
+  // recount height oof items and their child by order
+  reCountHeight = (e) => {
+    console.log('--treeIndex',e);
+ 
+    if(this.container.current){
+     
+      const ElemCollection = this.container.current.getElementsByClassName('rst__tree')[0].children;
+
+      const arrHeight = [];
+      const getChildHeight = (it) => {
+        // TODO get list of all 
+        it.forEach(it=>{
+          arrHeight.push(it.height);
+          if(Array.isArray(it.children) && it.children.length){
+            getChildHeight(it.children);
+          }
+
+        })
+      }
+      
+      console.log('--arrHeight', arrHeight);
+
+      getChildHeight(this.state.treeData);
+      
+
+
+
+      //set height
+      for(let i=0;ElemCollection.length>i;i++){
+        ElemCollection[i].style.height = `${arrHeight[i]}px`;
+      }
+
+      console.log('----------');
+    }
+  }
+
+
+
   render() {
+    // console.log('--toggleExpandedForAll',toggleExpandedForAll); func - открыть все
+    console.log('--FileExplorerTheme1', FileExplorerTheme1);
     return (
       <div 
         // style={{width: '500px'}}
       >
-        <h2>Добавление стилей</h2>
+        <h2>Разная высота</h2>
         <DndProvider backend={dndBackend}>
           <div>
             <span>
@@ -36,12 +102,23 @@ class Ex3 extends Component {
             </span>
 
             {/* для auto-height - этот параметр */}
-            <div style={{ height: '100%' }}> 
+            <div ref={this.container} style={{ height: '100%' }}> 
               <SortableTree
+                // function recount
+                generateNodeProps={(e)=>{
+                  // ГЛИЧ
+                  if(this.container.current){
+                    this.container.current.getElementsByClassName('rst__tree')[0].children[e.treeIndex].style.height = `${e.node.height}px`;
+                  }
+                }}
+
                 isVirtualized={false} // для auto-height - этот параметр
                 treeData={this.state.treeData}
-                onChange={treeData => this.setState({ treeData })}
-                
+                onChange={treeData => {
+                  this.setState({ treeData })
+                }}
+                theme={FileExplorerTheme2}
+
               />
             </div>
           </div>
--- 001 webpack/034 react-dnd/components/ex0.js
+++ 001 webpack/034 react-dnd/components/ex0.js
@@ -1,15 +1,108 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react'
+import ReactDOM from 'react-dom'
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import { useDrop, useDrag, DndProvider } from 'react-dnd'
 
+// !!! touch support (ннужно перезагрузить страницу)
+const isTouchDevice = !!('ontouchstart' in window || navigator.maxTouchPoints);
+const dndBackend = isTouchDevice ? TouchBackend : HTML5Backend;
 
-class Ex0 extends Component {
-  render() {
-    return (
-      <div>
+export default function Ex0() {
+  return (
+    <div>
         <h2>Работает тач (но нужно перезагрузить страницу) - базовый макет</h2>
+        <DndProvider backend={dndBackend} options={{ enableMouseEvents: true }}>
+          <Container />
+        </DndProvider>
 
-      </div>
-    );
+    </div>
+  )
+ }
+
+ const Dustbin = () => {
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: ItemTypes.BOX,
+    drop: () => {
+      return { name: 'Dustbin' }
+      },
+    collect: (monitor) => {
+      return {
+        isOver: monitor.isOver(),
+        canDrop: monitor.canDrop(),
+      }
+    },
+  })
+ const isActive = canDrop && isOver
+  let backgroundColor = '#222'
+  if (isActive) {
+   backgroundColor = 'darkgreen'
+  } else if (canDrop) {
+   backgroundColor = 'darkkhaki'
   }
+  return (
+   <div ref={drop} style={{
+    height: '12rem',
+    width: '12rem',
+    marginRight: '1.5rem',
+    marginBottom: '1.5rem',
+    color: 'white',
+    padding: '1rem',
+    textAlign: 'center',
+    fontSize: '1rem',
+    lineHeight: 'normal',
+    float: 'left',
+    backgroundColor
+   }}>
+    {isActive ? 'Release to drop' : 'Drag a box here'}
+   </div>
+  )
+ }
+ const Box = ({ name }) => {
+  const [{ isDragging }, drag] = useDrag({
+   item: { name, type: ItemTypes.BOX },
+   end: (item, monitor) => {
+    const dropResult = monitor.getDropResult()
+    if (item && dropResult) {
+     alert(`You dropped ${item.name} into ${dropResult.name}!`)
+    }
+   },
+   collect: (monitor) => ({
+    isDragging: monitor.isDragging(),
+   }),
+  })
+  const opacity = isDragging ? 0.4 : 1
+  return (
+   <div ref={drag} style={{
+    border: '1px dashed gray',
+    backgroundColor: 'white',
+    padding: '0.5rem 1rem',
+    marginRight: '1.5rem',
+    marginBottom: '1.5rem',
+    cursor: 'move',
+    float: 'left',
+    opacity
+   }}>
+    {name}
+   </div>
+  )
+ }
+ 
+ export const Container = memo(function Container() {
+  return (
+   <div>
+    <div style={{ overflow: 'hidden', clear: 'both' }}>
+     <Dustbin />
+    </div>
+    <div style={{ overflow: 'hidden', clear: 'both' }}>
+     <Box name="Glass" />
+     <Box name="Banana" />
+     <Box name="Paper" />
+    </div>
+   </div>
+  )
+ })
+ 
+const ItemTypes = {
+  BOX: 'box',
 }
-
-export default Ex0;
