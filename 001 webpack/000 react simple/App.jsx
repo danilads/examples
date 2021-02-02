c1887484e0a203ae17758a05a537ca860093e69c
danilads <vfhn3fghtkm>
2021-02-02 14:37:57 +0300

    upd

---
+++ 001 webpack/000 react simple/App.jsx
@@ -0,0 +1,28 @@
+﻿// react
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import 'babel-polyfill';
+
+import Main from './Main';
+// import {Navigation} from './components/Navigation';
+// import List from './components/pages/List';
+
+// redux
+// import { Provider } from 'react-redux';
+
+// css
+// import './scss/main.scss';
+
+// store
+// import {store} from './redux/store';
+
+// history
+// import { createBrowserHistory } from 'history'
+
+ReactDOM.render(
+  
+	<Main />
+	, document.getElementById('container') 
+);
+
---
+++ 001 webpack/000 react simple/Main.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import './Main.scss';
+class Main extends React.PureComponent {
+
+  	render() {
+		return (
+			<div className={"Main"}>
+				hello
+			</div>
+		);
+
+  	}
+
+}
+
+
+
+export default Main;
