abf286d09cbc38b46b192e8613ca94ad7aca75f8
danilads <daniilshemetau@gmail.com>
2021-05-05 21:27:57 +0300

    upd

--- 001 webpack/039 telegram bot/index.js
+++ 001 webpack/039 telegram bot/index.js
@@ -1,29 +1,7 @@
-const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
+const { Telegraf } = require('telegraf')
+const bot = new Telegraf('ТОКЕН') //сюда помещается токен, который дал botFather
+bot.start((ctx) => ctx.reply('Welcome')) //ответ бота на команду /start
+bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
+bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
+bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
+bot.launch() // запуск бота
